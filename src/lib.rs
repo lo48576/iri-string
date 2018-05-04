23049e17d5027e6d4e9f866b8db8ee524740c8d0
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-05-04 23:13:43 +0900

    Suppress clippy warning `derive_hash_xor_eq`

--- src/lib.rs
+++ src/lib.rs
@@ -129,6 +129,10 @@ fn validate_iri_str<S: AsRef<str>>(s: S) -> Result<S, UrlParseError> {
 
 
 /// IRI string slice.
+// derive_hash_xor_eq (clippy lint):
+//  This does not matter because explicit `PartialEq` impls are only doing
+//  type conversion (`&AbsoluteIriStr` to `&str`) and equalness is preserved.
+#[allow(unknown_lints, derive_hash_xor_eq)]
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, OpaqueTypedefUnsized)]
 #[cfg_attr(feature = "serde", derive(Serialize))]
 #[repr(C)]
@@ -256,6 +260,10 @@ impl<'de> Deserialize<'de> for &'de AbsoluteIriStr {
 
 
 /// Owned IRI string.
+// derive_hash_xor_eq (clippy lint):
+//  This does not matter because explicit `PartialEq` impls are only doing
+//  type conversion (`&AbsoluteIriString` to `&str`) and equalness is preserved.
+#[allow(unknown_lints, derive_hash_xor_eq)]
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, OpaqueTypedef)]
 #[cfg_attr(feature = "serde", derive(Serialize))]
 #[opaque_typedef(
