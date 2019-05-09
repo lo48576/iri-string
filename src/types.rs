0ee421770984338f1287779fbd7ff366d6071df6
YOSHIOKA Takuma <lo48576@hard-wi.red>
2019-05-09 18:42:31 +0900

    Describe about comparison traits a little in doc comments

--- src/types.rs
+++ src/types.rs
@@ -37,6 +37,9 @@
 //! * `AsRef<BarStr> for FooStr`
 //! * `AsRef<BarStr> for FooString`
 //! * `From<FooString> for BarString`
+//! * `PartialEq<FooStr> for BarStr` and lots of impls like that
+//!     + `PartialEq` and `ParitalOrd`.
+//!     + Slice, owned, `Cow`, reference, etc...
 //!
 //! # URI types
 //!
