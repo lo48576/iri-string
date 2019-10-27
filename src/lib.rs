98109ab79b90fa1ee271625c7d957cb94361ee4c
YOSHIOKA Takuma <lo48576@hard-wi.red>
2019-10-27 21:50:04 +0900

    Improve crate doc and add a note about IRIs comparison

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
-//! String types for [IRI](https://tools.ietf.org/html/rfc3987).
+//! String types for [RFC 3987 IRI][RFC 3987].
+//!
+//! Note that this crate does not have any extra knowledge about protocols.
+//! Comparisons between IRI strings by `PartialEq` and `Eq` is implemented as [simple string
+//! comparison](https://tools.ietf.org/html/rfc3986#section-6.2.1).
+//! You should implement by yourself or use another crate to use such extra knowledge to compare
+//! IRIs.
+//!
+//! [RFC 3987]: https://tools.ietf.org/html/rfc3987
 #![warn(missing_docs)]
 #![warn(clippy::missing_docs_in_private_items)]
 
