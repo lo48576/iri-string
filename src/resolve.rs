4b4f502c09ad2d251de708e46dc7d3454b744603
YOSHIOKA Takuma <lo48576@hard-wi.red>
2022-01-08 06:56:18 +0900

    Remove unnecessary usage of closure

--- src/resolve.rs
+++ src/resolve.rs
@@ -1066,7 +1066,7 @@ mod tests {
                             .find('/')
                             .map_or_else(|| input.len(), |pos| pos + 1)
                     } else {
-                        input.find('/').unwrap_or_else(|| input.len())
+                        input.find('/').unwrap_or(input.len())
                     };
                     output.extend(input.drain(..first_seg_end));
                 }
