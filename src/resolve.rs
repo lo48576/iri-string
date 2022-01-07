a417470d0e45b6e9719e7f8e1a626266870028b1
YOSHIOKA Takuma <lo48576@hard-wi.red>
2022-01-08 03:16:11 +0900

    Add a test that compares the new resolver and the naive RFC 3986 impl
    They should always output the same result.

--- src/resolve.rs
+++ src/resolve.rs
@@ -629,6 +629,9 @@ mod tests {
 
     use crate::types::{IriReferenceStr, IriStr};
 
+    #[cfg(feature = "alloc")]
+    use self::refimpl::resolve as resolve_refimpl;
+
     /// Test cases for strict resolvers.
     // [(base, [(input, output)])]
     const TEST_CASES: &[(&str, &[(&str, &str)])] = &[
@@ -834,6 +837,30 @@ mod tests {
 
     #[test]
     #[cfg(feature = "alloc")]
+    fn test_resolve_standalone_same_result_as_reference_impl() {
+        for (base, pairs) in TEST_CASES {
+            let base = <&IriStr>::try_from(*base)
+                .expect("Invalid testcase: base IRI should be absolute IRI");
+            for (input, expected) in *pairs {
+                let input = <&IriReferenceStr>::try_from(*input)
+                    .expect("Invalid testcase: `input` should be IRI reference");
+                let got = resolve(input, base);
+                assert_eq!(
+                    AsRef::<str>::as_ref(&got),
+                    *expected,
+                    "base = {:?}, input = {:?}",
+                    base,
+                    input
+                );
+
+                let referernce_result = resolve_refimpl(input, base);
+                assert_eq!(got, referernce_result);
+            }
+        }
+    }
+
+    #[test]
+    #[cfg(feature = "alloc")]
     fn test_fixed_base_resolver() {
         for (base, pairs) in TEST_CASES {
             let base = <&IriStr>::try_from(*base)
@@ -923,5 +950,174 @@ mod tests {
             .write_to_byte_slice(&mut buf)
             .expect("`buf` should be long enough");
         assert_eq!(result, "http://example.com/foo/bar");
+    }
+
+    /// Reference implementation based on RFC 3986 section 5.
+    #[cfg(feature = "alloc")]
+    mod refimpl {
+        use alloc::format;
+        use alloc::string::String;
+
+        use crate::parser::RiReferenceComponents;
+        use crate::spec::Spec;
+        use crate::types::{RiReferenceStr, RiStr, RiString};
+
+        /// Resolves the relative IRI.
+        ///
+        /// See <https://datatracker.ietf.org/doc/html/rfc3986#section-5.2.2>.
+        pub(super) fn resolve<S: Spec>(
+            reference: &RiReferenceStr<S>,
+            base: &RiStr<S>,
+        ) -> RiString<S> {
+            let r = RiReferenceComponents::from(reference);
+            let b = RiReferenceComponents::from(base.as_ref());
+
+            let t_scheme: &str;
+            let t_authority: Option<&str>;
+            let t_path: String;
+            let t_query: Option<&str>;
+            let t_fragment: Option<&str>;
+
+            if let Some(r_scheme) = r.scheme {
+                t_scheme = r_scheme;
+                t_authority = r.authority;
+                t_path = remove_dot_segments(r.path.into());
+                t_query = r.query;
+            } else {
+                if r.authority.is_some() {
+                    t_authority = r.authority;
+                    t_path = remove_dot_segments(r.path.into());
+                    t_query = r.query;
+                } else {
+                    if r.path.is_empty() {
+                        t_path = b.path.into();
+                        if r.query.is_some() {
+                            t_query = r.query;
+                        } else {
+                            t_query = b.query;
+                        }
+                    } else {
+                        if r.path.starts_with('/') {
+                            t_path = remove_dot_segments(r.path.into());
+                        } else {
+                            t_path =
+                                remove_dot_segments(merge(b.path, r.path, b.authority.is_some()));
+                        }
+                        t_query = r.query;
+                    }
+                    t_authority = b.authority;
+                }
+                t_scheme = b.scheme.expect("non-relative IRI must have a scheme");
+            }
+            t_fragment = r.fragment;
+
+            let s = recompose(t_scheme, t_authority, &t_path, t_query, t_fragment);
+            RiString::<S>::try_from(s).expect("resolution result must be a valid IRI")
+        }
+
+        /// Merges the two paths.
+        ///
+        /// See <https://datatracker.ietf.org/doc/html/rfc3986#section-5.2.3>.
+        fn merge(base_path: &str, ref_path: &str, base_authority_defined: bool) -> String {
+            if base_authority_defined && base_path.is_empty() {
+                format!("/{}", ref_path)
+            } else {
+                let base_path_end = base_path.rfind('/').map_or(0, |s| s + 1);
+                format!("{}{}", &base_path[..base_path_end], ref_path)
+            }
+        }
+
+        /// Removes dot segments from the path.
+        ///
+        /// See <https://datatracker.ietf.org/doc/html/rfc3986#section-5.2.4>.
+        fn remove_dot_segments(mut input: String) -> String {
+            let mut output = String::new();
+            while !input.is_empty() {
+                if input.starts_with("../") {
+                    // 2A.
+                    input.drain(..3);
+                } else if input.starts_with("./") {
+                    // 2A.
+                    input.drain(..2);
+                } else if input.starts_with("/./") {
+                    // 2B.
+                    input.replace_range(..3, "/");
+                } else if input == "/." {
+                    // 2B.
+                    input.replace_range(..2, "/");
+                } else if input.starts_with("/../") {
+                    // 2C.
+                    input.replace_range(..4, "/");
+                    remove_last_segment_and_preceding_slash(&mut output);
+                } else if input == "/.." {
+                    // 2C.
+                    input.replace_range(..3, "/");
+                    remove_last_segment_and_preceding_slash(&mut output);
+                } else if input == "." {
+                    // 2D.
+                    input.drain(..1);
+                } else if input == ".." {
+                    // 2D.
+                    input.drain(..2);
+                } else {
+                    // 2E.
+                    let first_seg_end = if let Some(after_slash) = input.strip_prefix('/') {
+                        // `+1` is the length of the initial slash.
+                        after_slash
+                            .find('/')
+                            .map_or_else(|| input.len(), |pos| pos + 1)
+                    } else {
+                        input.find('/').unwrap_or_else(|| input.len())
+                    };
+                    output.extend(input.drain(..first_seg_end));
+                }
+            }
+
+            output
+        }
+
+        /// Removes the last path segment and the preceding slash if any.
+        ///
+        /// See <https://datatracker.ietf.org/doc/html/rfc3986#section-5.2.4>,
+        /// step 2C.
+        fn remove_last_segment_and_preceding_slash(output: &mut String) {
+            match output.rfind('/') {
+                Some(slash_pos) => {
+                    output.drain(slash_pos..);
+                }
+                None => output.clear(),
+            }
+        }
+
+        /// Recomposes the components.
+        ///
+        /// See <https://datatracker.ietf.org/doc/html/rfc3986#section-5.3>.
+        fn recompose(
+            scheme: &str,
+            authority: Option<&str>,
+            path: &str,
+            query: Option<&str>,
+            fragment: Option<&str>,
+        ) -> String {
+            let mut result = String::new();
+
+            result.push_str(scheme);
+            result.push(':');
+            if let Some(authority) = authority {
+                result.push_str("//");
+                result.push_str(authority);
+            }
+            result.push_str(path);
+            if let Some(query) = query {
+                result.push('?');
+                result.push_str(query);
+            }
+            if let Some(fragment) = fragment {
+                result.push('#');
+                result.push_str(fragment);
+            }
+
+            result
+        }
     }
 }
