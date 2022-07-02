8b719c9d982f1a1fc8cc4e401ad7c52c0db677e9
YOSHIOKA Takuma <lo48576@hard-wi.red>
2022-07-02 15:45:22 +0900

    Remove deprecated `percent_encoding` module and aliases in it

--- src/lib.rs
+++ src/lib.rs
@@ -140,29 +140,3 @@ pub mod spec;
 mod tests;
 pub mod types;
 pub mod validate;
-
-/// Renamed to [`percent_encode`] module.
-///
-/// This module is provided with the old name just to keep backward compatibility.
-#[deprecated(since = "0.5.6", note = "renamed to `percent_encode` module")]
-pub mod percent_encoding {
-    use crate::percent_encode;
-
-    /// Renamed to [`percent_encode::PercentEncoded`].
-    #[deprecated(since = "0.5.6", note = "renamed to `percent_encode::PercentEncoded`")]
-    pub type PercentEncoded<T, S> = percent_encode::PercentEncoded<T, S>;
-
-    /// Renamed to [`percent_encode::PercentEncodedForIri`].
-    #[deprecated(
-        since = "0.5.6",
-        note = "renamed to `percent_encode::PercentEncodedForIri`"
-    )]
-    pub type PercentEncodedForIri<T> = percent_encode::PercentEncodedForIri<T>;
-
-    /// Renamed to [`percent_encode::PercentEncodedForUri`].
-    #[deprecated(
-        since = "0.5.6",
-        note = "renamed to `percent_encode::PercentEncodedForUri`"
-    )]
-    pub type PercentEncodedForUri<T> = percent_encode::PercentEncodedForUri<T>;
-}
